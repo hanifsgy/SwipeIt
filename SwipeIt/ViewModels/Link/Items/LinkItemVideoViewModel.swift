aaece6d2d7c65c0bcb87a999c0ea474070ff8a0c
Ivan Bruel <ivan.bruel@gmail.com>
2016-09-13 18:12:15 +0100

    Added embeded video compatibility

--- SwipeIt/ViewModels/Link/Items/LinkItemVideoViewModel.swift
+++ SwipeIt/ViewModels/Link/Items/LinkItemVideoViewModel.swift
@@ -12,13 +12,26 @@ import RxSwift
 // MARK: Properties and initializer
 class LinkItemVideoViewModel: LinkItemViewModel {
 
+  private static let webMetaTag =
+  "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1\">"
+
   // MARK: Public Properties
   let imageURL: NSURL?
   let videoURL: NSURL
+  let videoHTML: String?
 
   override init(user: User, accessToken: AccessToken, link: Link, showSubreddit: Bool) {
     imageURL = link.imageURL
     videoURL = link.url
+    if let embeddedMedia = link.secureEmbeddedMedia {
+      let iFrame = embeddedMedia.content.stringByDecodingHTMLEntities
+      let videoHTML = iFrame
+        .stringByReplacingOccurrencesOfString("\"\(embeddedMedia.width)\"", withString: "\"100%\"")
+        .stringByReplacingOccurrencesOfString("\"\(embeddedMedia.height)\"", withString: "\"100%\"")
+      self.videoHTML = "<head>\(LinkItemVideoViewModel.webMetaTag)</head><body>\(videoHTML)</body>"
+    } else {
+      self.videoHTML = nil
+    }
     super.init(user: user, accessToken: accessToken, link: link, showSubreddit: showSubreddit)
   }
 }
---
+++ SwipeIt/Views/Cards/LinkVideoCardView.swift
@@ -0,0 +1,76 @@
+//
+//  LinkVideoCarView.swift
+//  SwipeIt
+//
+//  Created by Ivan Bruel on 13/09/16.
+//  Copyright © 2016 Faber Ventures. All rights reserved.
+//
+
+import UIKit
+import TextStyle
+import WebKit
+import TTTAttributedLabel
+
+class LinkVideoCardView: LinkCardView {
+
+  override var viewModel: LinkItemViewModel? {
+    didSet {
+      guard let videoViewModel = viewModel as? LinkItemVideoViewModel,
+        videoHTML = videoViewModel.videoHTML else { return }
+      webView.loadHTMLString(videoHTML, baseURL: nil)
+    }
+  }
+
+
+  // MARK: - Views
+  private lazy var webView: WKWebView = self.createWebView()
+
+  // MARK - Initializers
+  override init() {
+    super.init(frame: .zero)
+    commonInit()
+  }
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    commonInit()
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    commonInit()
+  }
+
+  private func commonInit() {
+    contentView = webView
+  }
+
+  override func didAppear() {
+    super.didAppear()
+  }
+
+  override func didDisappear() {
+    super.didDisappear()
+  }
+}
+
+extension LinkVideoCardView {
+
+  private func createWebView() -> WKWebView {
+    let config = WKWebViewConfiguration()
+    let bodyStyle = "body { margin:0; }"
+    let source = "var node = document.createElement(\"style\"); node.innerHTML = \"\(bodyStyle)\";document.body.appendChild(node);"
+
+    let script = WKUserScript(
+      source: source,
+      injectionTime: .AtDocumentEnd,
+      forMainFrameOnly: false
+    )
+
+    config.userContentController.addUserScript(script)
+    let webView = WKWebView(frame: .zero, configuration: config)
+    webView.scrollView.scrollEnabled = false
+    return webView
+  }
+  
+}
