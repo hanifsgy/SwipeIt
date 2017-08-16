530206784a7b06f2a5416d978d522a0b10358fd3
Ivan Bruel <ivan.bruel@gmail.com>
2017-08-16 02:03:38 +0100

    Improved Network logging

--- SwipeIt/Networking/Network.swift
+++ SwipeIt/Networking/Network.swift
@@ -12,28 +12,42 @@ import RxSwift
 
 class Network {
 
-  fileprivate static var provider = RxMoyaProvider<RedditAPI>(endpointClosure: {
-    target -> Endpoint<RedditAPI> in
+  fileprivate static var provider = RxMoyaProvider<RedditAPI>(endpointClosure: { target -> Endpoint<RedditAPI> in
     return Endpoint<RedditAPI>(url: target.url,
-      sampleResponseClosure: { .networkResponse(200, target.sampleData) },
-      method: target.method,
-      parameters: target.parameters,
-      parameterEncoding: target.parameterEncoding,
-      httpHeaderFields: target.headers)
-    }, plugins: [
-      //NetworkLoggerPlugin(cURL: true),
-      credentialsPlugin
+                               sampleResponseClosure: { .networkResponse(200, target.sampleData) },
+                               method: target.method,
+                               parameters: target.parameters,
+                               parameterEncoding: target.parameterEncoding,
+                               httpHeaderFields: target.headers)
+  }, plugins: [
+    NetworkLoggerPlugin(verbose: true, cURL: true, output: networkPrint, responseDataFormatter: jsonFormatter),
+    credentialsPlugin
     ])
 
   fileprivate static var credentialsPlugin = CredentialsPlugin { target -> URLCredential? in
     guard let target = target as? RedditAPI else { return nil }
     return target.credentials
   }
+
+  private static func jsonFormatter(data: Data) -> Data {
+    do {
+      let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+      return try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
+    } catch {
+      return data
+    }
+  }
+
+  private static func networkPrint(separator: String, terminator: String, items: Any...) {
+    items.forEach { item in
+      print("\n\(String(describing: item))")
+    }
+  }
 }
 
 // MARK: Public Methods
 extension Network {
-
+  
   static func request(_ target: RedditAPI) -> Observable<Response> {
     return provider.request(target)
   }
